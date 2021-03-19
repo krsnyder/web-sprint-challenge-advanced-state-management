6957fcbfb6e3b6a64a1f6229820953ce90da9a76
Kirk <kirkrobertsnyder@gmail.com>
2021-03-19 10:04:22 -0400

    Added ADD_SMURF and ERROR reducer cases

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -24,13 +24,19 @@ const reducer = (state = initialState, action) => {
         errorMessage: action.payload,
         loading: false
       })
+    case ("ADD_SMURF"):
+      return ({
+        ...state,
+        smurfs: [...state.smurfs, action.payload]
+      })
+    case ("ERROR"):
+      return ({
+        ...state,
+        errorMessage: action.payload
+      })
     default:
       return(state)
   }
 }
 
 export default reducer;
-
-//5. Add in a reducer cases to accomidate the failed smurf api fetch.
-//6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
-//7. Add in a reducer case that adds in a value to the error message.
