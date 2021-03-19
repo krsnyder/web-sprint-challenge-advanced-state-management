c423354d25f980a5014e13c18ccc8cc2ca5ae43a
Kirk <kirkrobertsnyder@gmail.com>
2021-03-19 09:55:09 -0400

    Updated BEGIN_FETCH case and added FETCH_SUCCESS case

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,10 +7,16 @@ export const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case ("SMURF_FETCH"):
+    case ("BEGIN_FETCH"):
       return ({
         ...state,
-        smurfs: action.payload
+        loading: true
+      })
+    case ("FETCH_SUCCESS"):
+      return ({
+        ...state,
+        smurfs: action.payload,
+        loading: false
       })
     default:
       return(state)
@@ -19,8 +25,6 @@ const reducer = (state = initialState, action) => {
 
 export default reducer;
 
-//3. Add in a reducer case to accomidate the start of a smurf fetch.
-//4. Add in a reducer case to accomidate the successful smurf api fetch.
 //5. Add in a reducer cases to accomidate the failed smurf api fetch.
 //6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
 //7. Add in a reducer case that adds in a value to the error message.
